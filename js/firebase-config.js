8faefa3b791b646ab81c7ea3b1356644cda3169b
kshatriyaparichayak-star <kshatriyaparichayak@gmail.com>
2026-04-20 15:02:12 +0530

    update firebase-config.js

--- js/firebase-config.js
+++ js/firebase-config.js
@@ -1,3 +1,8 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-app.js";
+import { getAuth, GoogleAuthProvider } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-auth.js";
+import { getFirestore } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-firestore.js";
+
+// 1. आपकी फायरबेस कॉन्फ़िग
 export const firebaseConfig = {
     apiKey: "AIzaSyC0eEvmS3TYtFDfTjSI5mL9cfUmO3gt23I",
     authDomain: "kshatriya-parichay.firebaseapp.com",
@@ -6,3 +11,11 @@ export const firebaseConfig = {
     messagingSenderId: "952092943473",
     appId: "1:952092943473:web:38d14a98f28df05922a7d0"
 };
+
+// 2. फायरबेस को चालू करें
+const app = initializeApp(firebaseConfig);
+
+// 3. यहाँ से हम ज़रूरी टूल्स को एक्सपोर्ट कर रहे हैं ताकि दूसरी फाइलें इस्तेमाल कर सकें
+export const auth = getAuth(app);
+export const db = getFirestore(app);
+export const provider = new GoogleAuthProvider();
